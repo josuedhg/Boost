996d0a08f104a5f74292ae7784746932c93c970b
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2013-10-28 23:26:01 -0600

    comments are update

--- Bind/source/main.cpp
+++ Bind/source/main.cpp
@@ -26,11 +26,11 @@ int main ()
 	boost::function< int(void) > f=boost::bind< int >(boost_function_test, 4, 2);
 	std::cout<<"Boost Bind with funtions f() = "<<f()<<std::endl;
 	
-	//! Bind with a funtion
+	//! Bind with a struct
 	boost::function< int(void) > structures=boost::bind< int >(structure_test(), 4, 2);
 	std::cout<<"Boost Bind with structures structures() = "<<structures()<<std::endl;
 	
-	//! Bind with a funtion
+	//! Bind with a class
 	boost::function< bool(void) > classes=boost::bind< bool >(class_test(), 4, 4);
 	std::cout<<"Boost Bind with classes classes() = "<<classes()<<std::endl;
 	
